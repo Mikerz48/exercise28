a8cd411bddbef3d7550089e83eade5de10b5c315
Mikerz48 <michaelrenna48@gmail.com>
2023-07-04 16:46:29 +0200

    Done

--- exercise.js
+++ exercise.js
@@ -2,5 +2,4 @@ function sum(...nums) {
   return nums.reduce((acc, curr) => acc + curr, 0);
 }
 
-const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(...nums));
